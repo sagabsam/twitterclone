d488db8b9ab96090ef3e65af35dd04ac46de994e
sagabsam <201240001065@unisnu.ac.id>
2023-07-09 16:28:16 +0700

    Side Drawer UI & Navigation to Current User Profile

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/svg.dart';
-
+import 'package:twitter_clone/features/home/widgets/side_drawer.dart';
 import '../../../constants/assets_constants.dart';
 import '../../../constants/ui_constants.dart';
 import '../../../theme/pallete.dart';
@@ -45,6 +45,7 @@ class _HomeViewState extends State<HomeView> {
           size: 28,
         ),
       ),
+      drawer: const SideDrawer(),
       bottomNavigationBar: CupertinoTabBar(
         currentIndex: _page,
         onTap: onPageChange,
